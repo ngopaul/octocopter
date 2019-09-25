84144de5d6104eb2bc40ba6f336d02c477c5c2ec
Paul Ngo <paulngo123@gmail.com>
2019-09-24 21:02:12 -0700

    first commit

---
+++ test_pwm.py
@@ -0,0 +1,26 @@
+from board import SCL, SDA
+import busio
+import time
+
+from adafruit_pca9685 import PCA9685
+
+i2c_bus = busio.I2C(SCL, SDA)
+
+pca = PCA9685(i2c_bus)
+
+pca.frequency = 80
+
+user_input = 0
+
+while user_input != "":
+	user_input = input("New percent pwm cycle: ")
+	try:
+		pca.channels[0].duty_cycle = int(65535*float(user_input))
+	except Exception as e:
+		print("Error", e)
+	if user_input == "s1":
+		for i in range(50):
+			pca.channels[0].duty_cycle = 0xefff
+			time.sleep(0.05)
+			pca.channels[0].duty_cycle = 0x0fff
+pca.channels[0].duty_cycle = 0x0000
