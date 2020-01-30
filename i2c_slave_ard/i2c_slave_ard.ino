d2516f38d777c7d7f9b73f1f04197b84b1bde902
Paul Ngo <paulngo123@gmail.com>
2020-01-30 14:26:29 -0800

    update code for ard slave, updates variable which is referenced in main loop for control

--- i2c_slave_ard/i2c_slave_ard.ino
+++ i2c_slave_ard/i2c_slave_ard.ino
@@ -1,22 +1,38 @@
 #include <Wire.h>
 
+int MODE = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(0x8);
 
   Wire.onReceive(receiveEvent);
-
-  
+  Serial.begin(9600);
+    
 }
 
 void receiveEvent(int howMany) {
   while (Wire.available()) {
-    char c = Wire.read();
-    Serial.print(c);
+    int i = Wire.read();
+//    Serial.print(i);
+//    Serial.print("\n");
+    MODE = i;
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  switch(MODE) {
+    case 0:
+      Serial.print(0);
+      break;
+     case 1:
+      Serial.print(1);
+      break;
+     case 2:
+      Serial.print(2);
+      break;
+  }
+  Serial.print("\n");
   delay(100);
 }
